ed142f6d67d360316398884622749ed007f5ef7d
RashP07 <116902118+RashP07@users.noreply.github.com>
2023-12-01 12:04:04 +0530

    add skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,6 +6,7 @@ const Skills = () => {
             <div className='max-w-screen-lg max-h-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-auto'>
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 border-gray-400'>Skills</p>
+                    <p className='py-6'>These are the technologies I've worked with</p>
                     </div>
             </div>
         </div>
